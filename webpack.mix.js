7c995f89e515e550b6df789ca11f0d79e7150542
camzu1998 <kamillanger4@gmail.com>
2022-01-16 03:05:59 +0100

    CSS Versioning & some fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,4 +18,6 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
- 
+if (mix.inProduction()) {
+    mix.version();
+}
