63775c4044207c1af5185dcdafe63445416d1d1d
camzu1998 <kamillanger4@gmail.com>
2022-01-25 23:44:29 +0100

    Working on friendship tests

--- tests/Feature/Authenticated/FriendshipTest.php
+++ tests/Feature/Authenticated/FriendshipTest.php
@@ -47,11 +47,93 @@ class FriendshipTest extends AuthenticatedTestCase
         $response->assertStatus(200)->assertJson(['status' => 0]);
     }
     
-    // /** @test */
-    public function check_auth_user_update_friendship_status_route()
+    /** @test */
+    public function check_auth_user_can_accept_friendship_invite()
     {
-        $response = $this->put('/friendship/1', []);
+        $user = User::factory()->create();
+        $this->assertModelExists($user);
+        $friendship = Friendship::factory()->create([
+            'user_id' => $user->id,
+            'user2_id' => $this->user->id,
+            'by_who' => $user->id,
+            'status' => 0
+        ]);
+        $this->assertModelExists($friendship);
+        $response = $this->put('/friendship/'.$friendship->user_id, [
+            'button' => 'acceptInvite'
+        ]);
+        $response->assertStatus(200)->assertJson(['status' => 0]);
+        $this->assertDatabaseHas('friendship', [
+            'user_id' => $user->id,
+            'user2_id' => $this->user->id,
+            'status' => 1
+        ]);
+    }
+    
+    /** @test */
+    public function check_auth_user_can_deceline_friendship_invite()
+    {
+        $user = User::factory()->create();
+        $this->assertModelExists($user);
+        $friendship = Friendship::factory()->create([
+            'user_id' => $user->id,
+            'user2_id' => $this->user->id,
+            'by_who' => $user->id,
+            'status' => 0
+        ]);
+        $this->assertModelExists($friendship);
+        $response = $this->put('/friendship/'.$friendship->user_id, [
+            'button' => 'decelineInvite'
+        ]);
+        $response->assertStatus(200)->assertJson(['status' => 0]);
+        $this->assertDatabaseHas('friendship', [
+            'user_id' => $user->id,
+            'user2_id' => $this->user->id,
+            'status' => 2
+        ]);
+    }
 
-        $response->assertStatus(200)->assertJson(['status' => 9]);
+    /** @test */
+    public function check_auth_user_can_cancel_friendship_invite()
+    {
+        $user = User::factory()->create();
+        $this->assertModelExists($user);
+        $friendship = Friendship::factory()->create([
+            'user_id' => $this->user->id,
+            'user2_id' => $user->id,
+            'by_who' => $this->user->id,
+            'status' => 0
+        ]);
+        $this->assertModelExists($friendship);
+        $response = $this->put('/friendship/'.$friendship->user2_id, [
+            'button' => 'cancelInvite'
+        ]);
+        $response->assertStatus(200)->assertJson(['status' => 0]);
+        $this->assertDatabaseMissing('friendship', [
+            'user_id' => $this->user->id,
+            'user2_id' => $user->id
+        ]);
+    }
+
+    /** @test */
+    public function check_auth_user_can_delete_friendship()
+    {
+        $user = User::factory()->create();
+        $this->assertModelExists($user);
+        $friendship = Friendship::factory()->create([
+            'user_id' => $this->user->id,
+            'user2_id' => $user->id,
+            'by_who' => $this->user->id,
+            'status' => 1
+        ]);
+        $this->assertModelExists($friendship);
+        $response = $this->put('/friendship/'.$friendship->user2_id, [
+            'button' => 'deleteFriendship'
+        ]);
+        $response->assertStatus(200)->assertJson(['status' => 0]);
+        $this->assertDatabaseMissing('friendship', [
+            'user_id' => $this->user->id,
+            'user2_id' => $user->id
+        ]);
     }
 }
