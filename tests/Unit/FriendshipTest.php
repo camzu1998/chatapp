af62bbc85e3b4dc7b4b345d8990eb4dac1ea34a9
camzu1998 <kamillanger4@gmail.com>
2022-01-27 00:27:42 +0100

    Friendships refactoring

--- tests/Unit/FriendshipTest.php
+++ tests/Unit/FriendshipTest.php
@@ -2,17 +2,92 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Friendship;
+use App\Models\User;
 
 class FriendshipTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_check_get_user_friendships()
     {
-        $this->assertTrue(true);
+        //Prepare Friendship
+        $friends = User::factory()->count(2)->create();
+        $user = User::factory()->create();
+        //Make friendship
+        foreach($friends as $friend){
+            $friendship = Friendship::factory()->create([
+                'user_id' => $user->id,
+                'user2_id' => $friend->id,
+                'by_who' => $user->id
+            ]);
+            $this->assertModelExists($friendship);
+        }
+        
+        $friendships = Friendship::user($user->id)->get();
+        $this->assertEquals(count($friendships), 2);
+    }
+
+    public function test_check_if_friendship_exist()
+    {
+        //Prepare Friendship
+        $friend_one = User::factory()->create();
+        $friend_two = User::factory()->create();
+        //Make friendship
+        $friendship = Friendship::factory()->create([
+            'user_id' => $friend_one->id,
+            'user2_id' => $friend_two->id,
+            'by_who' => $friend_one->id
+        ]);
+        $this->assertModelExists($friendship);
+        $friendships = Friendship::check($friend_one->id, $friend_two->id);
+        $this->assertEquals($friendship->created_at, $friendships[0]->created_at);
+    }
+
+    public function test_set_status_friendships()
+    {
+        //Prepare Friendship
+        $friend_one = User::factory()->create();
+        $friend_two = User::factory()->create();
+        //Make friendship
+        $friendship = Friendship::factory()->create([
+            'user_id' => $friend_one->id,
+            'user2_id' => $friend_two->id,
+            'by_who' => $friend_one->id
+        ]);
+        $this->assertModelExists($friendship);
+        $friendships = Friendship::set_status($friend_one->id, $friend_two->id, 1);
+        $this->assertDatabaseHas('friendship', [
+            'user_id' => $friend_one->id,
+            'user2_id' => $friend_two->id,
+            'status' => 1
+        ]);
+    }
+
+    public function test_delete_friendship()
+    {
+        //Prepare Friendship
+        $friend_one = User::factory()->create();
+        $friend_two = User::factory()->create();
+        //Make friendship
+        $friendship = Friendship::factory()->create([
+            'user_id' => $friend_one->id,
+            'user2_id' => $friend_two->id,
+            'by_who' => $friend_one->id
+        ]);
+        $this->assertModelExists($friendship);
+        Friendship::delete_friendship($friend_one->id, $friend_two->id);
+        $this->assertDatabaseMissing('friendship', [
+            'user_id' => $friend_one->id,
+            'user2_id' => $friend_two->id,
+        ]);
     }
 }
