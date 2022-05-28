d709721db59d435831336a0b606e2bea83a32644
Kamil Langer <kamillanger4@gmail.com>
2022-05-28 18:28:35 +0200

    refactor: UserObserver

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,60 +2,32 @@
 
 namespace App\Observers;
 
+use App\Http\Controllers\UserSettingsController;
 use App\Models\User;
 
 class UserObserver
 {
-    /**
-     * Handle the User "created" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
     public function created(User $user)
     {
-        //
+        $user_settings_controller = new UserSettingsController();
+        $user_settings_controller->set_init_settings($user->id);
     }
 
-    /**
-     * Handle the User "updated" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
     public function updated(User $user)
     {
         //
     }
 
-    /**
-     * Handle the User "deleted" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
     public function deleted(User $user)
     {
         //
     }
 
-    /**
-     * Handle the User "restored" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
     public function restored(User $user)
     {
         //
     }
 
-    /**
-     * Handle the User "force deleted" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
     public function forceDeleted(User $user)
     {
         //
